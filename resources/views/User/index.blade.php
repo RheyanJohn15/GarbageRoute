2ff982e26be7964d235f814a8e9f3676ed2f383a
Tshmytzn <128598268+Tshmytzn@users.noreply.github.com>
2024-10-11 06:01:40 +0800

    Removed background backdrops, Fixed UI; recommended, uploaded sample photo for testing.

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -10,11 +10,80 @@
 
     <div class="main-banner" id="top">
         <div class="container">
-            <div class="row">
+            <div class="row p-0">
                 <div class="col-lg-12 me-4">
                     <div class="row">
-                        <div class="col-lg-6 align-self-center">
-                            <div class="owl-carousel owl-banner">
+                        <div class="col-lg-6 ms-4">
+
+                            <div class="owl-carousel owl-banner" style="margin-top: -15%; margin-left: -11%">
+                                <div class="item header-text">
+                                    <p style="color: rgb(0, 83, 0); font-size:30px; font-weight: bold">Silay City
+                                        Garbage <span style="color: rgb(0, 67, 105)"> Complaint Form</span>
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-12">
+                                <form id="contact" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <fieldset>
+                                                <input type="text" name="comp_name" id="comp_name"
+                                                    placeholder="Full Name" required>
+                                            </fieldset>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <fieldset>
+                                                <input type="email" name="email" id="email"
+                                                    pattern="[^ @]*@[^ @]*" placeholder="Your Email" required>
+                                            </fieldset>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <fieldset>
+                                                <input type="text" name="contact" id="contacts"
+                                                    placeholder="Contact" required>
+                                            </fieldset>
+                                        </div>
+                                        <div class="col-lg-12 mb-2 mt-2">
+                                            <fieldset>
+                                                <select class="form-select" name="nature" id="nature" required>
+                                                    <option value="" disabled selected> Select Nature of Complaint
+                                                    </option>
+                                                    <option>Missed Collection</option>
+                                                    <option>Late Irregular Service</option>
+                                                    <option>Improper Handling of Waste</option>
+                                                </select>
+                                            </fieldset>
+                                        </div>
+                                        <div class="col-lg-12 mb-4 mt-2">
+                                            <fieldset>
+                                                <textarea class="form-control" id="message" name="remarks" placeholder="Message/Remarks" rows="5" required></textarea>
+                                            </fieldset>
+                                        </div>
+                                        <div class="col-lg-12 mb-4">
+                                            <label for="attachment" class="form-label">Attach a Photo
+                                                (Optional):</label>
+                                            <input type="file" class="form-control" name="attachment" id="attachment"
+                                                accept=".jpg,.jpeg,.png" onchange="previewImage(event)">
+
+                                            <div id="imagePreview" class="mt-2" style="display:none;">
+                                                <img id="preview" src="" alt="Image Preview" class="img-fluid"
+                                                    style="width: 200px; height: 200px; object-fit: cover;" />
+
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <fieldset>
+                                                <button type="submit" id="form-submit" class="main-button">Submit
+                                                    Complaint</button>
+                                            </fieldset>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                            {{-- <div class="owl-carousel owl-banner">
                                 <div class="item header-text">
                                     <h6>Welcome to SCWMS</h6>
                                     <h2>For a <em>Cleaner</em>, and <span style="color: green">Greener</span> World</h2>
@@ -26,7 +95,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
                 </div>
@@ -34,19 +103,12 @@
         </div>
     </div>
 
-    <div id="services" class="our-services section">
-        <div class="services-right-dec">
-            <img src="assets/images/services-right-dec.png" alt="">
-        </div>
+    <div id="services" class="our-services section" style="margin-top: -10%">
         <div class="container">
-            <div class="services-left-dec">
-                <img src="assets/images/services-left-dec.png" alt="">
-            </div>
             <div class="row">
                 <div class="col-lg-6 offset-lg-3">
                     <div class="section-heading">
-                        <h2 style="font-size: 40px">Sustainable <em> Waste </em> Management <span> Solutions</span></h2>
-                        <span>Our Services</span>
+                        <h2 style="font-size: 40px">Sustainable <em> Waste </em> Management <em> Solutions</em></h2>
                     </div>
                 </div>
             </div>
@@ -55,51 +117,93 @@
                     <div class="owl-carousel owl-services">
 
                         <div class="item">
-                            <h4>Waste Segregation</h4>
-                            <div class="icon"><i class="fas fa-recycle icon-large"></i></div>
-                            <p>Promotes proper segregation of waste into biodegradable, non-biodegradable, and
-                                recyclable categories.</p>
+                            <h4>Complaint ID: 1</h4>
+                            <img src="ComplaintAssets/Complaint3.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> John Doe<br>
+                                <strong>Nature:</strong> Waste Collection Delay<br>
+                                <strong>Status:</strong> Resolved<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Waste Collection</h4>
-                            <div class="icon"><i class="fas fa-trash icon-large"></i></div>
-                            <p>Regular collection of waste from households and businesses, ensuring timely transport to
-                                disposal sites.</p>
+                            <h4>Complaint ID: 2</h4>
+                            <img src="ComplaintAssets/Complaint4.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Jane Smith<br>
+                                <strong>Nature:</strong> Noise Complaint<br>
+                                <strong>Status:</strong> In Progress<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Recycling Programs</h4>
-                            <div class="icon"><i class="fas fa-recycle icon-large"></i></div>
-                            <p>Encourages recycling initiatives by setting up materials recovery facilities to process
-                                recyclable waste.</p>
+                            <h4>Complaint ID: 3</h4>
+                            <img src="ComplaintAssets/Complaint3.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Alex Johnson<br>
+                                <strong>Nature:</strong> Pothole Issue<br>
+                                <strong>Status:</strong> Not Resolved<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Solid Waste Disposal</h4>
-                            <div class="icon"><i class="fas fa-dumpster icon-large"></i></div>
-                            <p>Safe disposal of solid waste at designated landfills, ensuring compliance with
-                                environmental standards.</p>
+                            <h4>Complaint ID: 4</h4>
+                            <img src="ComplaintAssets/Complaint4.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Emily Davis<br>
+                                <strong>Nature:</strong> Street Light Outage<br>
+                                <strong>Status:</strong> Resolved<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Waste Management</h4>
-                            <div class="icon"><i class="fas fa-exclamation-triangle icon-large"></i></div>
-                            <p>Special handling and disposal of hazardous waste materials to prevent contamination.</p>
+                            <h4>Complaint ID: 5</h4>
+                            <img src="ComplaintAssets/Complaint5.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Michael Brown<br>
+                                <strong>Nature:</strong> Graffiti Removal<br>
+                                <strong>Status:</strong> In Progress<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Public Education Campaigns</h4>
-                            <div class="icon"><i class="fas fa-book-open icon-large"></i></div>
-                            <p>Initiatives aimed at educating the public on sustainable waste management practices.</p>
+                            <h4>Complaint ID: 6</h4>
+                            <img src="ComplaintAssets/Complaint6.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Sarah Wilson<br>
+                                <strong>Nature:</strong> Fallen Tree Branch<br>
+                                <strong>Status:</strong> Not Resolved<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Climate Change Adaptation</h4>
-                            <div class="icon"><i class="fas fa-globe-americas icon-large"></i></div>
-                            <p>Integrates climate adaptation strategies in waste management to reduce the environmental
-                                impact.</p>
+                            <h4>Complaint ID: 7</h4>
+                            <img src="ComplaintAssets/Complaint7.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> David Lee<br>
+                                <strong>Nature:</strong> Water Leak<br>
+                                <strong>Status:</strong> Resolved<br>
+                            </p>
                         </div>
+
                         <div class="item">
-                            <h4>Flood Mitigation</h4>
-                            <div class="icon"><i class="fas fa-water icon-large"></i></div>
-                            <p>Addresses waste-related issues that contribute to flooding and landslides in vulnerable
-                                areas.</p>
+                            <h4>Complaint ID: 8</h4>
+                            <img src="ComplaintAssets/Complaint8.jpg" alt="Complaint Image" class="img-fluid"
+                                style="width: 350px; height: 300px; object-fit: cover;">
+                            <p class="text-start m-4">
+                                <strong>Name:</strong> Jessica Taylor<br>
+                                <strong>Nature:</strong> Abandoned Vehicle<br>
+                                <strong>Status:</strong> In Progress<br>
+                            </p>
                         </div>
+
 
                     </div>
                 </div>
@@ -128,8 +232,8 @@
                                             <i class="fas fa-recycle icon-large"></i>
                                         </div>
                                         <div class="count-digit">320</div>
-                                        <div class="count-title">Waste Management Initiatives</div>
-                                        <p>Reducing waste and enhancing recycling efforts.</p>
+                                        <div class="count-title">Pending</div>
+                                        <p>Complaints received and awaiting action.</p>
                                     </div>
                                 </div>
                             </div>
@@ -140,8 +244,8 @@
                                             <i class="fas fa-users icon-large"></i>
                                         </div>
                                         <div class="count-digit">640</div>
-                                        <div class="count-title">Community Engagement</div>
-                                        <p>Educating residents on sustainable practices.</p>
+                                        <div class="count-title">Scheduled for Action</div>
+                                        <p>Issues identified and actions scheduled.</p>
                                     </div>
                                 </div>
                             </div>
@@ -152,8 +256,8 @@
                                             <i class="fas fa-handshake icon-large"></i>
                                         </div>
                                         <div class="count-digit">120</div>
-                                        <div class="count-title">Successful Partnerships</div>
-                                        <p>Collaborating for a cleaner, greener community.</p>
+                                        <div class="count-title">Resolved</div>
+                                        <p>Complaints resolved with actions taken.</p>
                                     </div>
                                 </div>
                             </div>
@@ -187,12 +291,12 @@
             </div>
 
             <div class="row px-0" style="margin-left: 8%">
-                <div class="section-heading text-center mt-4">
+                {{-- <div class="section-heading text-center mt-4">
                     <h2 style="font-size: 40px !important">Silay Waste
                         <em>Management</em> Complaint <span style="font-size: 40px !important">Form</span>
                     </h2>
-                </div>
-                <!-- Form Row -->
+                </div> --}}
+                {{-- <!-- Form Row -->
                 <div class="col-lg-12">
                     <form id="contact" method="post" enctype="multipart/form-data">
                         @csrf
@@ -249,13 +353,10 @@
                             </div>
                         </div>
                     </form>
-                </div>
+                </div> --}}
             </div>
         </div>
 
-        <div class="contact-dec">
-            <img src="assets/images/contact-dec.png" alt="">
-        </div>
         <div class="contact-left-dec">
             <img src="assets/images/contact-left-dec.png" alt="">
         </div>
