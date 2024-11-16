429d7aca95dab9cd653518d542acb8458072e0e6
RheyanJohn <info@coresupporthub.com>
2024-11-16 23:44:11 +0800

    sms added

--- app/Services/V1/ComplaintsClass.php
+++ app/Services/V1/ComplaintsClass.php
@@ -1,9 +1,13 @@
 <?php
+
 namespace App\Services\V1;
+
 use App\Models\Complaints;
 use App\Services\ApiException;
 use App\Models\Zones;
-class ComplaintsClass{
+
+class ComplaintsClass
+{
     private $RESULT = null;
 
     public function __construct($method, $request)
@@ -11,20 +15,46 @@ class ComplaintsClass{
         $this->$method($request);
     }
 
-    private function submit($request){
+    private function submit($request)
+    {
 
         $attachment = $request->file('attachment');
 
-        if(!$attachment){
+        if (!$attachment) {
             throw new ApiException(ApiException::NO_IMAGE);
         }
-        if(!in_array($attachment->getClientOriginalExtension(), ['jpg', 'jpeg', 'png'])){
+        if (!in_array($attachment->getClientOriginalExtension(), ['jpg', 'jpeg', 'png'])) {
             throw new ApiException(ApiException::INVALID_PIC_TYPE);
         }
 
-        if($attachment->getSize() > 10485760){
+        if ($attachment->getSize() > 10485760) {
             throw new ApiException(ApiException::LARGE_IMAGE);
         }
+
+        $url = "https://semysms.net/api/3/sms.php";
+
+        $phone = $request->contact;
+        $msg = 'Hi, we have received your complaint and want to assure you that we are working to resolve it as quickly as possible. Thank you for your patience and understanding.';
+        $device = '348599';
+        $token = '25599652cf0f719e20d5f63db090219a';
+
+
+        $data = array(
+            "phone" => $phone,
+            "msg" => $msg,
+            "device" => $device,
+            "token" => $token
+        );
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        $output = curl_exec($curl);
+        curl_close($curl);
+
 
         $comp = new Complaints();
         $comp->comp_name = $request->comp_name;
@@ -36,47 +66,52 @@ class ComplaintsClass{
         $comp->comp_status = 0;
         $comp->save();
 
-        $fileName = "Complaint". $comp->comp_id. ".". $attachment->getClientOriginalExtension();
+        $fileName = "Complaint" . $comp->comp_id . "." . $attachment->getClientOriginalExtension();
         $attachment->move(public_path('ComplaintAssets/'), $fileName);
 
         $updateComp = Complaints::where('comp_id', $comp->comp_id)->first();
         $updateComp->update([
-            'comp_image' => $fileName 
+            'comp_image' => $fileName
         ]);
-        
+
         $this->RESULT = ['submit', 'Complaint Successfully Submitted', 'null'];
     }
 
-    private function list($request){
+    private function list($request)
+    {
         $complaints = Complaints::join('zones', 'zones.zone_id', '=', 'complaints.comp_zone')
-        ->select('complaints.*', 'zones.zone_name')
-        ->get();
+            ->select('complaints.*', 'zones.zone_name')
+            ->get();
 
         $this->RESULT = ['list', 'Complaint List', $complaints];
     }
 
-    private function remove($request){
+    private function remove($request)
+    {
         $comp = Complaints::where('comp_id', $request->comp_id)->first();
         $comp->delete();
 
         $this->RESULT = ['Remove Complaint', 'Complaint Successfully Deleted', 'null'];
     }
 
-    private function update($request){
+    private function update($request)
+    {
         $comp = Complaints::where('comp_id', $request->comp_id);
 
-        $comp->update(['comp_status'=>$request->status]);
+        $comp->update(['comp_status' => $request->status]);
 
         $this->RESULT = ['Update Complaint Status', 'Complaint is Successfully Updated', 'null'];
     }
 
-    private function getzone($req){
+    private function getzone($req)
+    {
         $zone = Zones::all();
 
-        $this->RESULT= ['getzone', "Zone Fetched", $zone];
+        $this->RESULT = ['getzone', "Zone Fetched", $zone];
     }
 
-    public function getResult(){
+    public function getResult()
+    {
         return $this->RESULT;
     }
 }
