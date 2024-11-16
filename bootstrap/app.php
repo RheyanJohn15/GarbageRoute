55b21ef7a9ecd70634dc9039a9def0a94c60714f
RheyanJohn <info@coresupporthub.com>
2024-11-17 00:01:07 +0800

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
-// ->registered(function ($app) {
-//     $app->usePublicPath(path: realpath(base_path('/../../silaywastemanagement')));
-// })
+->registered(function ($app) {
+    $app->usePublicPath(path: realpath(base_path('/../../silaywastemanagement')));
+})
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
